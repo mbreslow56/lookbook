82e491e48ed1278eb127140eb6d72063a3c99d9a
Danielle Carrick <daniellecarrick@Danielles-MacBook-Air.local>
2017-02-21 14:45:26 +0200

    it posts

--- lookbook/script.js
+++ lookbook/script.js
@@ -7,12 +7,21 @@ var fetch = function () {
     url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
     dataType: "json",
     success: function(data) {
-      console.log(data);
+      newResult(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
   });
 };
+
+var source = $('#search-template').html();
+var template = Handlebars.compile(source);
+
+
+var newResult = function(data) {
+  var newHTML = template({ title: data.items[0].volumeInfo.title , description: data.items[0].volumeInfo.description, author: data.items[0].volumeInfo.authors[0], image: data.items[0].volumeInfo.imageLinks.thumbnail });
+  $('.search-results').append(newHTML);
+}
 
 $('#search-btn').on('click', fetch);
